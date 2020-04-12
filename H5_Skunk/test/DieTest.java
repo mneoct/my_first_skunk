4c8258c08338764b6890246ff3d65a7a5990e17f
0800-Mervyn <neo05988@stthomas.edu>
2020-04-11 20:58:44 -0500

    no changes ignore

--- H5_Skunk/test/DieTest.java
+++ H5_Skunk/test/DieTest.java
@@ -1,11 +1,10 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Categories.ExcludeCategory;
 
-import edu.princeton.cs.introcs.StdOut;
 import skunk.Die;
 
 public class DieTest
